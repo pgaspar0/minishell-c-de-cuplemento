fea6766d9e3c1d7d36c7bbadba849af483b26df2
Paulo Gaspar <pgaspar@c1r4s9.42luanda.com>
2024-11-04 12:44:54 +0100

    Coisas Iniciais P

---
+++ minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pgaspar <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 08:01:11 by pgaspar           #+#    #+#             */
+/*   Updated: 2024/11/01 17:06:26 by pgaspar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "./libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
