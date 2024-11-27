080abcceb66daebf061f9c7e60dc6a3026387765
Jorge Carvalho <jorcarva@c1r5s10.42luanda.com>
2024-11-27 10:26:32 +0100

    hehehehehehehehehe

--- minishell.c
+++ minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pgaspar <pgaspar@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jorcarva <jorcarva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 08:00:27 by pgaspar           #+#    #+#             */
-/*   Updated: 2024/11/27 08:17:31 by pgaspar          ###   ########.fr       */
+/*   Updated: 2024/11/27 10:21:40 by jorcarva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,16 @@ int	main(void)
 	char	*concat;
 	char	**str3;
 	char	**str2;
+	char	**paulo;
 
 	str2 = ft_parse("ls|cat -e<<her_doc>file1>>file2");
 	concat = mat_concat(str2);
 	str3 = ft_parse2(concat);
+	paulo = ft_split(str3[3],' ');
 	for (int i = 0; str3[i]; i++)
 		printf("str3: %s\n", str3[i]);
+	for (int i = 0; paulo[i]; i++)
+		printf("heheheheh:%s\n", paulo[i]);
 	printf("string concat: %s\n", concat);
 	signal(3, SIG_IGN);
 	signal(SIGINT, handle_sigint);
