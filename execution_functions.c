430f113ef8201a7e2595b21ca611a0987b660c84
pgaspar <paulochiwecagaspar54@gmail.com>
2025-01-11 18:27:52 +0100

    ainda com problema nos pipes

--- execution_functions.c
+++ execution_functions.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execution_functions.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jorcarva <jorcarva@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pgaspar <pgaspar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 12:28:31 by pgaspar           #+#    #+#             */
-/*   Updated: 2025/01/10 20:17:42 by jorcarva         ###   ########.fr       */
+/*   Updated: 2025/01/11 18:27:07 by pgaspar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,10 @@ void	execute_commands(t_command *cmd_list, t_env **envs)
 	pid_t pid;
 	t_command *current;
 
-	int in_fd = 0; // Entrada inicial: STDIN
+	int in_fd = 0;
 	current = cmd_list;
 	while (current)
 	{
-		// Criação de pipe, se necessário
 		if (current->next && pipe(pipe_fd) == -1)
 		{
 			perror("Pipe error");
@@ -44,14 +43,14 @@ void	execute_commands(t_command *cmd_list, t_env **envs)
 			exit(1);
 		}
 		if (pid == 0)
-		{ // Processo filho
+		{
 			if (in_fd != 0)
-			{ // Redirecionar entrada, se necessário
+			{
 				dup2(in_fd, 0);
 				close(in_fd);
 			}
 			if (current->next)
-			{ // Redirecionar saída para o próximo comando
+			{
 				dup2(pipe_fd[1], 1);
 				close(pipe_fd[0]);
 				close(pipe_fd[1]);
@@ -73,31 +72,31 @@ void	execute_commands(t_command *cmd_list, t_env **envs)
 			}
 		}
 		else
-		{ // Processo pai
+		{
 			waitpid(pid, NULL, 0);
-			if (in_fd != 0) // Fechar entrada antiga
+			if (in_fd != 0)
 				close(in_fd);
 			if (current->next)
-			{ // Configurar entrada para o próximo comando
+			{
 				close(pipe_fd[1]);
 				in_fd = pipe_fd[0];
 			}
 		}
-		current = current->next; // Avançar para o próximo comando
+		current = current->next;
 	}
 } */
-void execute_commands_iterative(t_command *cmd_list, t_env **envs)
+
+/* void execute_commands_iterative(t_command *cmd_list, t_env **envs)
 {
     char **env_matrix;
     int pipe_fd[2];
     t_command *current;
 
-    int in_fd = 0; // Entrada inicial: STDIN
+    int in_fd = 0;
     current = cmd_list;
 
     while (current)
     {
-        // Criar pipe, se necessário
         if (current->next && pipe(pipe_fd) == -1)
         {
             perror("Pipe error");
@@ -106,10 +105,10 @@ void execute_commands_iterative(t_command *cmd_list, t_env **envs)
 
         if (is_builtin_command(current->args))
         {
-            // Redirecionar entrada e saída, se necessário
             int saved_stdin = dup(0);
             int saved_stdout = dup(1);
 
+            ft_putstr_fd("Chegou aqui\n", 1);
             if (in_fd != 0)
             {
                 dup2(in_fd, 0);
@@ -121,11 +120,9 @@ void execute_commands_iterative(t_command *cmd_list, t_env **envs)
                 close(pipe_fd[0]);
                 close(pipe_fd[1]);
             }
-
             handle_redirections(current->redirs, STDOUT_FILENO);
-            execute_builtin(current->args, envs); // Executa o built-in no processo principal
+            execute_builtin(current->args, envs);
 
-            // Restaurar descritores originais
             dup2(saved_stdin, 0);
             dup2(saved_stdout, 1);
             close(saved_stdin);
@@ -139,7 +136,6 @@ void execute_commands_iterative(t_command *cmd_list, t_env **envs)
         }
         else
         {
-            // Criar processo filho para comandos externos
             pid_t pid = fork();
             if (pid == -1)
             {
@@ -147,7 +143,7 @@ void execute_commands_iterative(t_command *cmd_list, t_env **envs)
                 exit(1);
             }
             if (pid == 0)
-            { // Processo filho
+            {
                 if (in_fd != 0)
                 {
                     dup2(in_fd, 0);
@@ -169,15 +165,109 @@ void execute_commands_iterative(t_command *cmd_list, t_env **envs)
                 exit(1);
             }
             else
-            { // Processo pai
+            {
                 waitpid(pid, NULL, 0);
             }
         }
 
-        // Gerenciar pipes
         if (in_fd != 0)
             close(in_fd);
         if (current->next && !is_builtin_command(current->args))
+        {
+            close(pipe_fd[1]);
+            in_fd = pipe_fd[0];
+        }
+
+        current = current->next;
+    }
+} */
+
+void execute_commands_iterative(t_command *cmd_list, t_env **envs)
+{
+    char **env_matrix;
+    int pipe_fd[2];
+    int in_fd = STDIN_FILENO;
+    t_command *current = cmd_list;
+
+    while (current)
+    {
+        if (current->next && pipe(pipe_fd) == -1)
+        {
+            perror("Pipe error");
+            exit(1);
+        }
+
+        if (is_builtin_command(current->args))
+        {
+            int saved_stdin = dup(STDIN_FILENO);
+            int saved_stdout = dup(STDOUT_FILENO);
+
+            if (in_fd != STDIN_FILENO)
+            {
+                dup2(in_fd, STDIN_FILENO);
+                close(in_fd);
+            }
+            if (current->next)
+            {
+                dup2(pipe_fd[1], STDOUT_FILENO);
+                close(pipe_fd[0]);
+                close(pipe_fd[1]);
+            }
+
+            handle_redirections(current->redirs, STDOUT_FILENO);
+            execute_builtin(current->args, envs);
+
+            dup2(saved_stdin, STDIN_FILENO);
+            dup2(saved_stdout, STDOUT_FILENO);
+            close(saved_stdin);
+            close(saved_stdout);
+
+            if (current->next)
+            {
+                close(pipe_fd[1]);
+                in_fd = pipe_fd[0];
+            }
+        }
+        else
+        {
+            pid_t pid = fork();
+            if (pid == -1)
+            {
+                perror("Fork error");
+                exit(1);
+            }
+            if (pid == 0)
+            {
+                if (in_fd != STDIN_FILENO)
+                {
+                    dup2(in_fd, STDIN_FILENO);
+                    close(in_fd);
+                }
+                if (current->next)
+                {
+                    dup2(pipe_fd[1], STDOUT_FILENO);
+                    close(pipe_fd[0]);
+                    close(pipe_fd[1]);
+                }
+
+                handle_redirections(current->redirs, STDOUT_FILENO);
+
+                env_matrix = env_to_matrix(*envs);
+                execve(get_caminho(ft_split(getenv("PATH"), ':'), current->args),
+                       current->args, env_matrix);
+                perror("Execve error");
+                free_matrix(env_matrix);
+                exit(1);
+            }
+            else
+            {
+                waitpid(pid, NULL, 0);
+            }
+        }
+
+        if (in_fd != STDIN_FILENO)
+            close(in_fd);
+        if (current->next)
         {
             close(pipe_fd[1]);
             in_fd = pipe_fd[0];
